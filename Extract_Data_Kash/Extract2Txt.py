5ec99d420798752232b0188627c55bb57597bd86
kashbari <kashbari@gmail.com>
2021-10-11 14:21:47 -0700

    kash add extract2txt, pdfmine,overview,readme, and sample pdf imgs

---
+++ Extract_Data_Kash/Extract2Txt.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+
+import pytesseract
+from pdf2image import convert_from_path 
+import os
+#import subprocess
+import pandas as pd
+
+## Set path where pdf files are
+path = "/home/kbari/Documents/Erdos/pdfminer_texts/"
+
+## PDF Miner pass
+def pdfminer_pass():
+    ''' PDF Miner.Six first pass; Calls bash script; requires it be executable (chmod +x pdfmine.sh)'''
+    os.system('./pdfmine.sh')
+    #subprocess.call(['sh',path+'pdfmine.sh'])
+    return
+
+## INC
+def check(thresh):
+    ''' Determine which files are did not get extracted with pdfminer; Checks
+    file size'''
+    return 
+
+## Tesseract OCR pass; INC
+def tesseract_pass(list_of_img_pdfs):
+    ''' Runs Tesseract OCR on list of pdfs '''
+    tesseract_parse = []
+    for file in list_of_img_pdfs:
+        out = convert_from_path(file,500)
+        text = ''    
+        for o in out:
+            text += str(py.tesseract.image_to_string(o))
+        tesseract_parse.append(text)
+    df_tesser = pd.DataFrame(tesseract_parse,columns=[list_of_img_pdfs])
+    return df_tesser
+
+
+##Convert from txt files to a dataframe; Other information to include possibly?
+def txt_to_df(path):
+    DIR = os.listdir(path)
+    raw_df_lst = []
+    #pd.read_table(file,header=None,quotechar=None,quoting=3,error_bad_lines=False) for file in DIR]
+    for i in range(len(DIR)):
+        with open(DIR[i]) as f:
+            lines = f.readlines()
+            data = '\n'.join(map(str,lines))
+            di = pd.DataFrame([data,len(lines),len(data)],index=['raw_text','num_lines','num_char'],columns=[DIR[i]]).T
+            raw_df_lst.append(di)
+    #print(len(raw_df_lst))
+    df_raw = pd.concat(raw_df_lst)
+    return df_raw
+
+
---
+++ Extract_Data_Kash/README.md
@@ -0,0 +1,31 @@
+## Table of Contents
+1. [General Info](#general-info)
+2. [Technologies](#technologies)
+3. [Installation](#installation)
+4. [Collaboration](#collaboration)
+5. [FAQs](#faqs)
+### General Info
+***
+Qarik Corporate Project for the Erdos Institute
+## Technologies
+***
+A list of technologies used within the project:
+* [Python](https://www.python.org/): Version 3.7 
+* [PDFMiner.six](https://pdfminersix.readthedocs.io/en/latest/): Version -- 
+* [Tesseract OCR](https://pypi.org/project/pytesseract/): Version 0.3.8
+## Installation/Implementation
+***
+A little intro about the installation. 
+```
+$ git clone https://example.com
+$ cd ../path/to/the/file
+$ npm install
+$ npm start
+```
+Side information: --
+## Collaboration
+***
+This project was worked on by Kashif Bari, Kevin Bombardier, Attilio Castano, Bingjin Liu.
+## FAQs
+***
+A list of frequently asked questions:
