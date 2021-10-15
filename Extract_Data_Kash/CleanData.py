017dc6b7b813d65a721806c3e33368d3bbfba199
kashbari <kashbari@gmail.com>
2021-10-15 10:06:33 -0700

    added to clean data, extracts loan amount and country

--- Extract_Data_Kash/CleanData.py
+++ Extract_Data_Kash/CleanData.py
@@ -9,21 +9,21 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 nltk.download('stopwords')
 nltk.download('wordnet')
-
 #nltk.download()
 
-
 ## Path to text files
-path = "/home/kbari/Documents/Erdos/pdfminer_texts/"
+path = "/home/kbari/git_repo/FinanceErdosProj/PyMuPdf_Text/"
+path1 = "/home/kbari/git_repo/FinanceErdosProj/Tesseract_Text/"
 
 ## Load from txt from files to a dataframe; Other information to include possibly?
+
+#pd.read_table(file,header=None,quotechar=None,quoting=3,error_bad_lines=False) for file in DIR]
 def txt_to_df(path):
     ''' Put all txt files into single dataframe'''
     DIR = os.listdir(path)
     raw_df_lst = []
-    #pd.read_table(file,header=None,quotechar=None,quoting=3,error_bad_lines=False) for file in DIR]
     for i in range(len(DIR)):
-        with open(DIR[i]) as f:
+        with open(path+DIR[i]) as f:
             lines = f.readlines()
             data = '\n'.join(map(str,lines))
             di = pd.DataFrame([data,len(data)],index=['raw_text','num_char'],columns=[DIR[i]]).T
@@ -44,6 +44,20 @@ def text_preprocess(text):
     nopunc = ''.join([i for i in nopunc if not i.isdigit()])
     nopunc =  [word.lower() for word in nopunc.split() if word not in stopwords.words('english')]
     return [stemmer.lemmatize(word) for word in nopunc]
+
+def loan_amount_preprocess(text):
+    ''' Grab Loan amount '''
+    start = 'ARTICLE II' 
+    end =  'ARTICLE III'
+    t1 = (text.split(start)[1].split(end)[0])
+    t = [s.translate(str.maketrans('','',string.punctuation)) for s in t1.split()]
+    n = [int(i) for i in t if i.isdigit()]
+    return max(n)
+
+def country_preprocess(text):
+    start = 'between'
+    end = 'and'
+    return text.split(start)[1].split(end)[0].strip()
 
 
 def main():
