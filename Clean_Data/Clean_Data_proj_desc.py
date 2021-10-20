a51dca2ac57902ae1fe4ec9086361fadc34b3f22
kashbari <kashbari@gmail.com>
2021-10-20 15:27:31 -0700

    added proj_desc file

---
+++ Clean_Data/Clean_Data_proj_desc.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+
+from itertools import *
+import os
+import numpy as np
+import pandas as pd
+import string
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+nltk.download('stopwords')
+nltk.download('wordnet')
+#nltk.download()
+
+## Path to text files
+path = "/home/kbari/git_repo/FinanceErdosProj/PyMuPdf_Text/"
+path1 = "/home/kbari/git_repo/FinanceErdosProj/Tesseract_Text/"
+
+## Load from txt from files to a dataframe; Other information to include possibly?
+
+#pd.read_table(file,header=None,quotechar=None,quoting=3,error_bad_lines=False) for file in DIR]
+def txt_to_df(path):
+    ''' Put all txt files into single dataframe'''
+    DIR = os.listdir(path)
+    raw_df_lst = []
+    for i in range(len(DIR)):
+        with open(path+DIR[i],encoding = "ISO-8859-1") as f:
+            lines = f.readlines()
+            data = '\n'.join(map(str,lines))
+            print(DIR[i])
+            p = proj_desc_preprocess(data)
+            di= pd.DataFrame([data,p,len(data)],index=['raw_text','proj_desc','num_char'],columns=[DIR[i]]).T
+            raw_df_lst.append(di)
+    #print(len(raw_df_lst))
+    df_raw = pd.concat(raw_df_lst)
+    return df_raw
+
+
+
+## Removes punctuation, stopwords, lemmatizes
+def text_preprocess(text):
+    '''
+    Remove all punctuation,stopwords, lemmatize -> returns list of words
+    '''
+    stemmer = WordNetLemmatizer()
+    nopunc = [char for char in text if char not in string.punctuation]
+    nopunc = ''.join([i for i in nopunc if not i.isdigit()])
+    nopunc =  [word.lower() for word in nopunc.split() if word not in stopwords.words('english')]
+    return [stemmer.lemmatize(word) for word in nopunc]
+    
+    
+    
+def proj_desc_preprocess(text):
+    ''' Grab Project Description'''
+    start = 'SCHEDULE II' 
+    end =  'SCHEDULE III'
+    try:
+        return text.split(start)[1].split(end)[0]
+    except:
+        return None
+
